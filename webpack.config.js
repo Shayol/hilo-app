2261b755f7d2fe15d3fba80c3610e469e84bb746
mila <vorga88@google.com>
2018-12-18 12:12:13 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'script.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        //exclude: /index\.html$/, //without this HtmlWebPackPlugin will not work with template file correctly
+        include: path.join(__dirname, 'src/html'),
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+              interpolate: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader', options: {
+              sourceMap: true,
+              ident: 'postcss',
+              plugins: () => [
+                require("autoprefixer")()
+              ]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/'
+          }
+        }
+      },
+      {
+        test: /\.(obj)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/'
+          }
+        }
+      },
+      {
+        test: /\.(ico)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: '[name].[ext]',
+            outputPath: './'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'Title',
+      template: "./src/html/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    })
+  ]
+};
