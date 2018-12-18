a50d5082e95c4631c3bf89b7d3a7918543718828
mila <vorga88@google.com>
2018-12-18 18:56:36 +0200

    add intersection observer for slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,38 @@
 import "../scss/main.scss";
+require('intersection-observer');
 
 window.addEventListener('load', function () {
     let sliderLogo = document.querySelectorAll(".logo");
     let info = document.querySelector('.info');
     let container = document.querySelector(".container");
+    let slider = document.querySelector(".slider__content");
+    let arrowDown = document.querySelector(".slider__arrow--down");
+    let slides = document.querySelectorAll(".slide");
+    let currentSlide = 1;
 
     sliderLogo.forEach(logo => {
         logo.addEventListener('click', () => {
             container.classList.toggle("show-info");
         });
     });
-})
+
+    //scroll on mobile
+    arrowDown.addEventListener('click', e => {
+        if (TweenLite) {
+            e.preventDefault();
+            TweenLite.to(slider, 1, { scrollTo: "#slide" + (parseInt(currentSlide) + 1) });
+        }
+    });
+
+    const intersectionObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.intersectionRatio > 0.3) {
+                currentSlide = entry.target.dataset.id;
+            }
+
+        });
+    }, { threshold: 0.6 });
+
+    slides.forEach((element) => intersectionObserver.observe(element));
+
+});
