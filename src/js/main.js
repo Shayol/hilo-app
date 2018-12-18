255ab403d0426d6df13c77338d5c59c81d26cd2e
mila <vorga88@google.com>
2018-12-18 18:10:25 +0200

    show info

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import "../scss/main.scss";
+
+window.addEventListener('load', function () {
+    let sliderLogo = document.querySelectorAll(".logo");
+    let info = document.querySelector('.info');
+    let container = document.querySelector(".container");
+
+    sliderLogo.forEach(logo => {
+        logo.addEventListener('click', () => {
+            container.classList.toggle("show-info");
+        });
+    });
+})
